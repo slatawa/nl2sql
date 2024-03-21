cd8420dac243972b6fba7c71bb600c00d1f5a5ef
sanchit latawa <sanchitlatawa@google.com>
2024-03-21 13:44:11 +0530

    make prompts dynamic
    make prompts dynamic

--- nl2sql_src/nl2sql_generic.py
+++ nl2sql_src/nl2sql_generic.py
@@ -5,6 +5,7 @@ import sqlalchemy
 import pandas as pd
 import langchain
 import sqlglot
+from nl2sql_src.prompts import *
 from langchain_google_vertexai import VertexAI
 from google.cloud import bigquery
 
@@ -121,16 +122,8 @@ class Nl2sqlBq:
             only_tables_info = only_tables_info + f"{table} | \
                 {self.metadata_json[table]['Description']}\n"
 
-        prompt = f'''
-You are a database expert at selecting a table from a list of tables based on their description.
-For the provided question choose what is the table_name most likely to be relevant.
-Only mention the table name from the following list and their description.
+        prompt = Table_filtering_prompt.format(only_tables_info = only_tables_info, question = question)
 
-Table name | description
-{only_tables_info}
-
-Question: {question}
-'''
         result = self.llm.invoke(prompt)
         #print(result)
 
@@ -226,31 +219,9 @@ Question: {question}
                 column_info = f"""{column["Name"]} \
                     ({column["Type"]}) : {column["Description"]}. {column["Examples"]}\n"""
                 columns_info = columns_info + column_info
-            table_str = f'''
-    Table Name : {table_json["Name"]}
-
-    Description: {table_json["Description"]}
-
-    This table has the following columns : 
-    {columns_info}
-    \n
-    '''
-            sql_prompt = f'''
-    Only use the following tables meta-data:
-
-    ```
-    {table_str}
-    ```
-
-    You are an SQL expert at generating SQL queries from a natural language question. Given the input question, create a syntactically correct Biguery query to run.
-
-    Only use the few relevant columns given the question.
-    Pay attention to use only the column names that you can see in the schema description. Be careful to not query for columns that do not exist. Also, pay attention to which column is in which table. Do not use more than 10 columns in the query. Focus on the keywords indicating calculation. 
-    Please think step by step and always validate the reponse.
-    recitify each column names by referencing them from the meta-data.
-
-    For this question what would be the most accurate SQL query?
-    Question: {question}'''
+            sql_prompt = Sql_Generation_prompt.format(table_name = table_json["Name"], 
+                                                      table_description = table_json["Description"],
+                                                      columns_info = columns_info, question = question)
             #print(sql_prompt)
             response = self.llm.invoke(sql_prompt)
             sql_query = response.replace('sql', '').replace('```', '')
@@ -311,21 +282,9 @@ Question: {question}
         """
         try:
             if insight:
-                prompt = f'''
-You are an expert Data Analyst. Given a report of SQL query and the question in
-natural language, provide a very insightful, intuitive and a not too long well-explained summary of the
-result which would help the user understand the result better and take informed decisions. 
-If the result does not have any data, then just mention that briefly in the summary.
-question: {question}
-result: {str(result)}'''
+                prompt = Result2nl_insight_prompt.format(question = question, result = str(result))
             else:
-                prompt = f'''
-You are an expert Data Analyst. Given a report of SQL query and the question in
-natural language, provide a very crisp, short, intuitive and easy-to-understand summary of the result.
-If the result does not have any data, then just mention that briefly in the summary.
-question: {question}
-result: {str(result)}
-'''
+                prompt = Result2nl_prompt.format(question = question, result = str(result))
 
             return self.llm.invoke(prompt)
         except Exception as exc:
@@ -346,16 +305,7 @@ result: {str(result)}
         and fits the NL description correctly,"No" otherwise.
         """
 
-        prompt = f'''
-You are an expert at validating SQL queries. Given the Natrual language description
-and the SQL query corresponding to that description, please check if the students answer is correct.
-There can be different ways to achieve the same result by forming the query differently.
-If the students SQL query matches the ground truth and fits the NL description correctly, then return yes
-else return no.
-Natural language description: {nl_description}
-Ground truth: {ground_truth}
-students answer: {llm_amswer}
-        '''
+        prompt = Auto_verify_sql_prompt.format(nl_description = nl_description, ground_truth = ground_truth, llm_amswer = llm_amswer)
         return self.llm.invoke(prompt)
 
     def batch_run(self,test_file_name, output_file_name,execute_query=False, 
--- nl2sql_src/prompts.py
+++ nl2sql_src/prompts.py
@@ -1,11 +1,57 @@
-SQL_GEN_PROMPTS = [
-    '''Only use the following tables meta-data:
+Table_filtering_prompt = """
+You are a database expert at selecting a table from a list of tables based on their description.
+For the provided question choose what is the table_name most likely to be relevant.
+Only mention the table name from the following list and their description.
+
+Table name | description
+{only_tables_info}
+
+Question: {question}
+"""
+
+Result2nl_insight_prompt = '''
+You are an expert Data Analyst. Given a report of SQL query and the question in
+natural language, provide a very insightful, intuitive and a not too long well-explained summary of the
+result which would help the user understand the result better and take informed decisions. 
+If the result does not have any data, then just mention that briefly in the summary.
+question: {question}
+result: {result}'''
+
+Result2nl_prompt = '''
+You are an expert Data Analyst. Given a report of SQL query and the question in
+natural language, provide a very crisp, short, intuitive and easy-to-understand summary of the result.
+If the result does not have any data, then just mention that briefly in the summary.
+question: {question}
+result: {result}
+'''
+
+Auto_verify_sql_prompt = '''
+You are an expert at validating SQL queries. Given the Natrual language description
+and the SQL query corresponding to that description, please check if the students answer is correct.
+There can be different ways to achieve the same result by forming the query differently.
+If the students SQL query matches the ground truth and fits the NL description correctly, then return yes
+else return no.
+Natural language description: {nl_description}
+Ground truth: {ground_truth}
+students answer: {llm_amswer}
+'''
+
+
+
+Sql_Generation_prompt = '''
+Only use the following tables meta-data:
 
 ```
-{metadata_json[tables_list[0]]}
+Table Name : {table_name}
+
+Description: {table_description}
+
+This table has the following columns : 
+{columns_info}
+\n
 ```
 
-You are an SQL expert at generating SQL queries from a natural language question. Given the input question, create a syntactically correct BigQuery query to run.
+You are an SQL expert at generating SQL queries from a natural language question. Given the input question, create a syntactically correct Biguery query to run.
 
 Only use the few relevant columns given the question.
 Pay attention to use only the column names that you can see in the schema description. Be careful to not query for columns that do not exist. Also, pay attention to which column is in which table. Do not use more than 10 columns in the query. Focus on the keywords indicating calculation. 
@@ -13,128 +59,8 @@ Please think step by step and always validate the reponse.
 recitify each column names by referencing them from the meta-data.
 
 For this question what would be the most accurate SQL query?
-Question: {question}''',
-
-    '''Only use the following tables meta-data:
-
-```
-{metadata_json[tables_list[0]]}
-```
-
-You are an SQL expert at generating SQL queries from a natural language question. Given the input question, create a syntactically correct BigQuery query to run.
-
-Only use the few relevant columns given the question.
-Pay attention to use only the column names that you can see in the schema description. Be careful to not query for columns that do not exist. Also, pay attention to which column is in which table. Do not use more than 10 columns in the query. Focus on the keywords indicating calculation. 
-Please think step by step and always validate the reponse.
-recitify each column names by referencing them from the meta-data.
-
-NOTE: Take a deep breath and double check all syntax are compatible with with BIGQUERY.
-
-For this question what would be the most accurate SQL query?
-Question: {question}''',
-
-    '''Only use the following tables meta-data:
-
-```
-{metadata_json[tables_list[0]]}
-```
-
-You are an SQL expert at generating SQL queries from a natural language question. Given the input question, create a syntactically correct BigQuery query to run.
-
-Only use the few relevant columns given the question.
-Pay attention to use only the column names that you can see in the schema description. Be careful to not query for columns that do not exist. Also, pay attention to which column is in which table. Do not use more than 10 columns in the query. Focus on the keywords indicating calculation. 
-Please think step by step and always validate the reponse.
-recitify each column names by referencing them from the meta-data.
-
-NOTE: Make sure all syntax are compatible with with BIGQUERY.
-
-For this question what would be the most accurate SQL query?
-Question: {question}''',
-
-    '''Only use the following tables meta-data:
-
-```
-{metadata_json[tables_list[0]]}
-```
-
-You are an SQL expert at generating SQL queries from a natural language question. Given the input question, create a syntactically correct BigQuery query to run.
-
-Only use the few relevant columns given the question.
-Pay attention to use only the column names that you can see in the schema description. Be careful to not query for columns that do not exist. Also, pay attention to which column is in which table. Do not use more than 10 columns in the query. Focus on the keywords indicating calculation. 
-Please think step by step and always validate the reponse.
-recitify each column names by referencing them from the meta-data.
-
-NOTE: Make sure all syntax are compatible with with BIGQUERY.
-Cast DATETIME to STRING before applying SUBSTR on DATETIME columns.
-
-For this question what would be the most accurate SQL query?
-Question: {question}'''
-]
+Question: {question}
+'''
 
 
-TABLE_FILTER_PROMPTS = [
-'''You are a database expert at selecting a table from a list of tables based on their description.
-    For the provided question choose what is the table_name most likely to be relevant.
-    Only mention the table name from the following list and their description-keywords. Mention multiple tables names if applicable.
 
-    Table name | description-keywords
-    authorizations_search | This table contains information about all the transactions in sales and their details like status, date, network, payment methods etc. Auth, Sale, Flow, Transaction
-    settlement_search | The Approved Transactions are now sent for Settlement. Settlement transactions are the first step in Funding the merchant. Keywords are Settled, Settlement, Settle
-    chargebacks_search | Chargebacks Search Table, Dispute, Chargeback
-    funding_search | Fund, Deposit
-    
-    Question: {question}
-    ''',
-
-'''You are a database expert at selecting a table from a list of tables based on their description.
-    For the provided question choose what is the table_name most likely to be relevant.
-    Only select the table from the following list and their description-keywords. Select multiple tables if applicable.
-
-    Table name | description-keywords
-    authorizations_search | This table contains information about all the transactions in sales and their details like status, date, network, payment methods etc. Auth, Sale, Flow, Transaction
-    settlement_search | The Approved Transactions are now sent for Settlement. Settlement transactions are the first step in Funding the merchant. Keywords are Settled, Settlement, Settle
-    chargebacks_search | Chargebacks Search Table, Dispute, Chargeback
-    funding_search | Fund, Deposit
-    
-    Question: {question}
-    '''
-]
-
-NL_EXPLAIN_PROMPTS = [
-'''You are an expert Data Analyst. Given a report of SQL query and the question in
-        natural language, provide a very insightful, intuitive and a not too long well-explained summary of the
-        result which would help the user understand the result better and take informed decisions. 
-        If the result does not have any data, then just mention that briefly in the summary.
-        question: {question}
-        result: {str(result)}''',
-
-'''You are an expert Data Analyst. Given a report of SQL query and the question in
-        natural language, provide a very crisp, short, intuitive and easy-to-understand summary of the result.
-        If the result does not have any data, then just mention that briefly in the summary.
-        question: {question}
-        result: {str(result)}'''
-]
-
-SQL_CORRECTION_PROMPTS = [
-'''You are an SQL expert at correcting SQL queries so that it is completely compatible 
-    with BigQuery and syntactically correct. If nothing needs to be changed then just give the query as it is.
-    If some functions are not present in buigquery then
-    replace them with functions which can be run in BigQuery.
-    Also Cast DATETIME to STRING before applying SUBSTR or any other string manipulation functions on DATETIME columns when needed.
-
-    For this sql what is the BigQuery compatible query?
-    SQL: {sql}''',
-
-]
-
-AUTO_VERIFY_PROMPTS = [
-'''You are an expert at validating SQL queries. Given the Natrual language description
-      and the SQL query corresponding to that description, please check if the students answer is correct.
-      There can be different ways to achieve the same result by forming the query differently.
-      If the students SQL query matches the ground truth and fits the NL description correctly, then return yes
-      else return no.
-      Natural language description: {nl_description}
-      Ground truth: {ground_truth}
-      students answer: {llm_amswer}
-    ''',
-]
